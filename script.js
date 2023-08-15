bd909ed08cc5299e7b8610ec13287da22abc5844
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-08-14 23:02:47 -0300

    Porcentagem

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
     const weight = parseFloat(document.getElementById('weight').value);
     const height = parseFloat(document.getElementById('height').value);
     
-    console.log(weight)
-    console.log(height)
+    if (isNaN(weight) || isNaN(height) || height === 0) {
+        document.getElementById('result').innerHTML = 'Por favor, digite valores válidos.';
+        return;
+    }
+    
+    const bmi = weight / (height * height);
+    const bmiFormatted = bmi.toFixed(2);
+
+    console.log(bmiFormatted)
 });
