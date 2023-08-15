64eebc02d359e29db0f43201c316e3fe40f676a4
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-08-14 22:47:41 -0300

    Values inputs

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('calculateBtn').addEventListener('click', function() {
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
+    
+    console.log(weight)
+    console.log(height)
+});
